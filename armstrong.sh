df0da3f48120638acadf2f23d8c8f0c9ed446981
ubuntu70 <aswath7862001>
2020-01-28 14:26:05 +0530

    dummy

---
+++ armstrong.sh
@@ -0,0 +1,19 @@
+echo "Enter a number"
+read c
+x=$c
+sum=0
+r=0
+n=0
+while [ $x -gt 0 ]
+do
+r=`expr $x % 10`
+n=`expr $r \* $r \* $r`
+sum=`expr $sum + $n`
+x=`expr $x / 10`
+done
+if [ $sum -eq $c ]
+then
+echo "The number is an armstrong number"
+else
+echo "The number is not an armstrong number"
+fi
